eea86dbce5b55497d590519a90f71309ae8dc0c5
Roman Sokolov <sokolovromann@gmail.com>
2022-01-15 17:23:01 +0300

    Fix WelcomeText

--- app/src/main/java/ru/sokolovromann/mynotepad/screens/welcome/components/WelcomeDisplay.kt
+++ app/src/main/java/ru/sokolovromann/mynotepad/screens/welcome/components/WelcomeDisplay.kt
@@ -12,6 +12,7 @@ import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import ru.sokolovromann.mynotepad.R
@@ -57,6 +58,7 @@ fun WelcomeDisplay(
                 text = stringResource(id = R.string.welcome_title),
                 color = MaterialTheme.colors.onSurface,
                 style = MaterialTheme.typography.h5,
+                textAlign = TextAlign.Center,
                 modifier = Modifier.padding(bottom = 16.dp)
             )
             DefaultTextButton(
